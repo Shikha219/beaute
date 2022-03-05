14b71bb27c76c145a1564879866eebf1baeda9af
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2022-03-05 18:31:08 +0530

    added global style and router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FinalProductsPageComponent } from './Pages/final-products-page/final-products-page.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { ProductsPageComponent } from './Pages/products-page/products-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component:HomePageComponent},
+  { path: "", component:ProductsPageComponent},
+  { path: "", component:FinalProductsPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
