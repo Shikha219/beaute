36521ac22f39cf0548cb464f7dee9d639d9f5e30
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2022-03-05 17:36:15 +0530

    project setup

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './Components/header-component/header-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
