ac2e25a45727636f31aa2fb75106d0e9865bf0ca
Shikha Suman <67411985+Shikha219@users.noreply.github.com>
2022-04-25 22:05:05 +0530

    final backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
-from fileinput import filename
 from flask import Flask, request, jsonify
 import werkzeug
+import time
+from model import predict
+
 
 app = Flask(__name__)
 
@@ -10,11 +12,16 @@ def upload():
     if(request.method == "POST"):
         imagefile = request.files['image']
         filename = werkzeug.utils.secure_filename(imagefile.filename)
-        imagefile.save("./uploadedimages/" + filename)
+        imagefile.save("./uploadedimages/" +
+                       time.strftime('%H%M%S') + filename)
+        msg = predict()
         return jsonify({
-            "message": "Image Uploaded Successfully"
+            "message": "image uploaded successfully",
+            "result": msg
+
         })
 
 
 if __name__ == "__main__":
+
     app.run(port=3000, debug=True)
